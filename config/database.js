f45d82c4ca39a7baedaff59542461656548c1c70
FriendlyUser <pandabeareee@gmail.com>
2018-03-26 19:49:44 -0700

    Added todo css, javascript files and connected to remote database

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    remoteUrl : 'mongodb://freetier:testing@ds149905.mlab.com:49905/sandbox_free_tier',
+    localUrl: 'mongodb://freetier:testing@ds149905.mlab.com:49905/sandbox_free_tier'
+};
+
